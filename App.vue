0e0a52ce933cf600d5f26b9dc8f394b14203222e
Romyl-hub <77636268+Romyl-hub@users.noreply.github.com>
2022-09-27 16:10:24 +0300

    Two

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+ <div id="app"></div>
+ <my-component/>
+</template>
+
+<script>
+import MyComponent from './components/Mycomponent.vue'
+import input from './components/input/input.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyComponent, 
+    input
+  }
+
+}
+</script>
+
+
---
+++ components/Mycomponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <div clas="root">
+        <label> 
+            Введите email:
+            <input
+             type="email"
+             :value="email"
+             @input="emailInput ($event.target.value)"
+             />
+            
+       </label>
+       <label> 
+            Введите Пароль:
+            <input
+             type="password" 
+            :value="password"
+            @input="passwordInput ($event.target.value)"
+            />
+       </label>
+       password:{{pasword}}
+       email:{{email}}
+       
+    </div>
+</template>
+<script>
+    export default {
+        name: 'MyComponent',
+        data() {
+            return {
+             email: 'warm@example',
+             password:'1231425'
+            }
+        },
+        methods:{
+            emailInput(value) {
+                this.email = value
+                
+            },
+            passwordInput(value) {
+                this.password = value
+            }
+
+        }
+    }
+</script>
